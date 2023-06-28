b174a8e8b6b6fdb927953732f0c7308a6ea1a868
Israill <cosynap@mail.ru>
2023-06-28 23:52:05 +0300

    настроил свайпер

---
+++ final-round/src/js/swiper.js
@@ -0,0 +1,35 @@
+import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper styles
+import "swiper/scss";
+import "swiper/scss/pagination";
+import "swiper/scss/navigation";
+
+  const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 15,
+  effect: "slide",
+  speed: 400,
+  slidesPerView: "auto",
+  // autoplay: {
+  //   stopOnLastSlide: true,
+  //   delay: 2000,
+  // },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".swiper-button-prev",
+    clickable: true,
+  },
+  modules: [Navigation, Pagination],
+  slidesPerView: "auto",
+  keyboardControl: true,
+  paginationClickable: true,
+  });
+  
